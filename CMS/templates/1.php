0f8ff54a83dc6ab5a3e7ad149e89338460e9da19
Venkatesh Akula <venkatesh1.akula@paytm.com>
2019-08-07 23:19:39 +0530

    My new project

--- CMS/templates/1.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$conn = mysql_connect("localhost","venkatesh","8309932239");
-	$db = mysql_select_db("paytm");
-?>
--- CMS/templates/2.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once('1.php');
-	$name = $_POST['name'];
-	$age = $_POST['age'];
-	if(mysql_query("INSERT INTO user VALUES('$name','$age')"))
-		echo "success";
-	else
-		echo "fail";
-?>
